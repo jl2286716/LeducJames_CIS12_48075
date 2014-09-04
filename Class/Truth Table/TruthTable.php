da3b076798968724abb230980a6ec98ed8fb851e
jl2286716 <jleduc@student.rcc.edu>
2014-09-03 22:40:43 -0700

    Signed-off-by: jl2286716 <jleduc@student.rcc.edu>

--- Class/Truth Table/TruthTable.php
+++ Class/Truth Table/TruthTable.php
@@ -13,23 +13,26 @@
     </head>
     
     <body>
-    	<?php // Echo out a heading.
-			echo "<h1>Boolean Truth Table</h1>";
+    	<?php // Echo out a page heading.
+			echo "<center><h1>Boolean Truth Table</h1></center>";
 		?>
-		<?php // Echo out the open table tag and headers.
-			echo "<table width='200' border='1'>";
-			echo "<tr><th colspan='2'>BOOLEAN</th><th colspan='2'><center>NOT</center></th>";
+		<?php // Echo out the open table tag and table headers.
+			echo "<center><table width='200' border='1'>";
+			
+			echo "<tr><th colspan='2'>Boolean</th><th colspan='2'><center>NOT</center></th>";
 			echo "<th><center>AND</center></th><th><center>OR</center></th>";
-			echo "<th colspan='3'><center>EXCLUSIVELY OR</center></th>";
-			echo "<th colspan='4'><center>DE MORGAN'S LAWS</center></th></tr>";
+			echo "<th colspan='3'><center>Exclusively OR</center></th>";
+			echo "<th colspan='4'><center>De Morgan's Laws</center></th></tr>";
+			
 			echo "<tr><th>X</th><th>Y</th><th>!X</th><th>!Y</th><th>X&&Y</th><th>X||Y</th>";
 			echo "<th>X^Y</th><th>X^Y^Y</th><th>X^Y^X</th>";
 			echo "<th>!(X&&Y)</th><th>!X||!Y</th><th>!(X||Y)</th><th>!X&&!Y</th></tr>";
 		?>
-	  	<?php // Row 1: T,T
+	  	<?php // Echo out row 1: T,T
 			$x=true;
 			$y=true;
-			// If/Else Statement Syntax eg: ('condition'?"value if true":"value if false")
+			// If/Then/Else Statement Syntax:
+				// ('condition'?'value if true':'value if false')
 			echo "<tr><td>".($x?"T":"F")."</td>";
 			echo "<td>".($y?"T":"F")."</td>";
 			echo "<td>".(!$x?"T":"F")."</td>";
@@ -44,7 +47,7 @@
 			echo "<td>".(!($x||$y)?"T":"F")."</td>";
 			echo "<td>".(!$x&&!$y?"T":"F")."</td></tr>";
 		?>
-	  	<?php // Row 2: T,F
+	  	<?php // Echo out row 2: T,F
 			$y=false;
 			echo "<tr><td>".($x?"T":"F")."</td>";
 			echo "<td>".($y?"T":"F")."</td>";
@@ -60,7 +63,7 @@
 			echo "<td>".(!($x||$y)?"T":"F")."</td>";
 			echo "<td>".(!$x&&!$y?"T":"F")."</td></tr>";
 		?>
-	  	<?php // Row 3: F,T
+	  	<?php // Echo out row 3: F,T
 			$x=false;
 			$y=true;
 			echo "<tr><td>".($x?"T":"F")."</td>";
@@ -77,7 +80,7 @@
 			echo "<td>".(!($x||$y)?"T":"F")."</td>";
 			echo "<td>".(!$x&&!$y?"T":"F")."</td></tr>";
 		?>
-	  	<?php // Row 4: F,F
+	  	<?php // Echo out row 4: F,F
 			$y=false;
 			echo "<tr><td>".($x?"T":"F")."</td>";
 			echo "<td>".($y?"T":"F")."</td>";
@@ -91,7 +94,17 @@
 			echo "<td>".(!($x&&$y)?"T":"F")."</td>";
 			echo "<td>".(!$x||!$y?"T":"F")."</td>";
 			echo "<td>".(!($x||$y)?"T":"F")."</td>";
-			echo "<td>".(!$x&&!$y?"T":"F")."</td></tr></table>"; // Close the table.
+			echo "<td>".(!$x&&!$y?"T":"F")."</td></tr>";
+			
+			// Close the table.
+			echo "</table></center>";
+		?>
+		<?php // Echo out notes.
+			// Exclusively OR
+			echo "<enter><p><i>Note: <b>Exclusively OR</b></i></p></center>";
+			echo "<enter><p><i>X^Y = true if different, false if same.</i></p></center>";
+			echo "<enter><p><i>X^Y^Y = the 'Y's cancel each other out.</i></p></center>";
+			echo "<enter><p><i>X^Y^X = the 'X's cancel each other out.</i></p></center></i></p>";
 		?>
     </body>
 </html>
