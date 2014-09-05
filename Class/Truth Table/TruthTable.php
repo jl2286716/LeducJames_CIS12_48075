4afca858ca7ab21e095394255a6127d4f4753fe2
jl2286716 <jleduc@student.rcc.edu>
2014-09-04 20:11:09 -0700

    Signed-off-by: jl2286716 <jleduc@student.rcc.edu>

--- Class/Truth Table/TruthTable.php
+++ Class/Truth Table/TruthTable.php
@@ -3,9 +3,10 @@
     <head>
     	<?php
 			/*
-				Programmer: James Leduc
-				Date: Sep 03, 2014
-				Purpose: Create Truth Table
+				Programmer: 	James Leduc
+				Date Created: 	Sep 03, 2014
+				Date Modified:	Sep 04, 2014
+				Purpose: 		Create Truth Table
 			*/
 		?>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -17,14 +18,15 @@
 			echo "<center><h1>Truth Table</h1></center>";
 		?>
 		<?php // Echo out the open table tag and table headers.
-			echo "<center><table width='200' border='1'>";
+			echo "<center><table width=auto border='1'>";
 			
-			echo "<tr><th colspan='2'>Boolean</th><th colspan='2'><center>NOT</center></th>";
+			echo "<tr><th rowspan='2'>X</th><th rowspan='2'>Y</th>";
+			echo "<th colspan='2'><center>NOT</center></th>";
 			echo "<th><center>AND</center></th><th><center>OR</center></th>";
 			echo "<th colspan='3'><center>Exclusively OR</center></th>";
-			echo "<th colspan='4'><center>De Morgan's Laws</center></th></tr>";
+			echo "<th colspan='4'><center>Augustus De Morgan's Laws</center></th></tr>";
 			
-			echo "<tr><th>X</th><th>Y</th><th>!X</th><th>!Y</th><th>X&&Y</th><th>X||Y</th>";
+			echo "<tr><th>!X</th><th>!Y</th><th>X&&Y</th><th>X||Y</th>";
 			echo "<th>X^Y</th><th>X^Y^Y</th><th>X^Y^X</th>";
 			echo "<th>!(X&&Y)</th><th>!X||!Y</th><th>!(X||Y)</th><th>!X&&!Y</th></tr>";
 		?>
@@ -32,85 +34,85 @@
 			$x=true;
 			$y=true;
 			
-			// If/Then/Else Statement Syntax:
+			// Ternary Operator (If/Then/Else Statement) Syntax:
 				// ('IF_condition'?'THEN_value_if_TRUE':'ELSE_value_if_FALSE')
-			echo "<tr><td>".($x?"T":"F")."</td>";
-			echo "<td>".($y?"T":"F")."</td>";
-			echo "<td>".(!$x?"T":"F")."</td>";
-			echo "<td>".(!$y?"T":"F")."</td>";
-			echo "<td>".($x&&$y?"T":"F")."</td>";
-			echo "<td>".($x||$y?"T":"F")."</td>";
-			echo "<td>".($x^$y?"T":"F")."</td>";
-			echo "<td>".($x^$y^$y?"T":"F")."</td>";
-			echo "<td>".($x^$y^$x?"T":"F")."</td>";
-			echo "<td>".(!($x&&$y)?"T":"F")."</td>";
-			echo "<td>".(!$x||!$y?"T":"F")."</td>";
-			echo "<td>".(!($x||$y)?"T":"F")."</td>";
-			echo "<td>".(!$x&&!$y?"T":"F")."</td></tr>";
+			echo "<tr><td><center>".($x?"T":"F")."</center></td>";
+			echo "<td><center>".($y?"T":"F")."</center></td>";
+			echo "<td><center>".(!$x?"T":"F")."</center></td>";
+			echo "<td><center>".(!$y?"T":"F")."</center></td>";
+			echo "<td><center>".($x&&$y?"T":"F")."</center></td>";
+			echo "<td><center>".($x||$y?"T":"F")."</center></td>";
+			echo "<td><center>".($x^$y?"T":"F")."</center></td>";
+			echo "<td><center>".($x^$y^$y?"T":"F")."</center></td>";
+			echo "<td><center>".($x^$y^$x?"T":"F")."</center></td>";
+			echo "<td><center>".(!($x&&$y)?"T":"F")."</center></td>";
+			echo "<td><center>".(!$x||!$y?"T":"F")."</center></td>";
+			echo "<td><center>".(!($x||$y)?"T":"F")."</center></td>";
+			echo "<td><center>".(!$x&&!$y?"T":"F")."</center></td></tr>";
 		?>
 	  	<?php // Echo out the T,F row.
 		 // $x is still true from former initialization.
 			$y=false;
 			
-			echo "<tr><td>".($x?"T":"F")."</td>";
-			echo "<td>".($y?"T":"F")."</td>";
-			echo "<td>".(!$x?"T":"F")."</td>";
-			echo "<td>".(!$y?"T":"F")."</td>";
-			echo "<td>".($x&&$y?"T":"F")."</td>";
-			echo "<td>".($x||$y?"T":"F")."</td>";
-			echo "<td>".($x^$y?"T":"F")."</td>";
-			echo "<td>".($x^$y^$y?"T":"F")."</td>";
-			echo "<td>".($x^$y^$x?"T":"F")."</td>";
-			echo "<td>".(!($x&&$y)?"T":"F")."</td>";
-			echo "<td>".(!$x||!$y?"T":"F")."</td>";
-			echo "<td>".(!($x||$y)?"T":"F")."</td>";
-			echo "<td>".(!$x&&!$y?"T":"F")."</td></tr>";
+			echo "<tr><td><center>".($x?"T":"F")."</center></td>";
+			echo "<td><center>".($y?"T":"F")."</center></td>";
+			echo "<td><center>".(!$x?"T":"F")."</center></td>";
+			echo "<td><center>".(!$y?"T":"F")."</center></td>";
+			echo "<td><center>".($x&&$y?"T":"F")."</center></td>";
+			echo "<td><center>".($x||$y?"T":"F")."</center></td>";
+			echo "<td><center>".($x^$y?"T":"F")."</center></td>";
+			echo "<td><center>".($x^$y^$y?"T":"F")."</center></td>";
+			echo "<td><center>".($x^$y^$x?"T":"F")."</center></td>";
+			echo "<td><center>".(!($x&&$y)?"T":"F")."</center></td>";
+			echo "<td><center>".(!$x||!$y?"T":"F")."</center></td>";
+			echo "<td><center>".(!($x||$y)?"T":"F")."</center></td>";
+			echo "<td><center>".(!$x&&!$y?"T":"F")."</center></td></tr>";
 		?>
 	  	<?php // Echo out the F,T row.
 			$x=false;
 			$y=true;
 			
-			echo "<tr><td>".($x?"T":"F")."</td>";
-			echo "<td>".($y?"T":"F")."</td>";
-			echo "<td>".(!$x?"T":"F")."</td>";
-			echo "<td>".(!$y?"T":"F")."</td>";
-			echo "<td>".($x&&$y?"T":"F")."</td>";
-			echo "<td>".($x||$y?"T":"F")."</td>";
-			echo "<td>".($x^$y?"T":"F")."</td>";
-			echo "<td>".($x^$y^$y?"T":"F")."</td>";
-			echo "<td>".($x^$y^$x?"T":"F")."</td>";
-			echo "<td>".(!($x&&$y)?"T":"F")."</td>";
-			echo "<td>".(!$x||!$y?"T":"F")."</td>";
-			echo "<td>".(!($x||$y)?"T":"F")."</td>";
-			echo "<td>".(!$x&&!$y?"T":"F")."</td></tr>";
+			echo "<tr><td><center>".($x?"T":"F")."</center></td>";
+			echo "<td><center>".($y?"T":"F")."</center></td>";
+			echo "<td><center>".(!$x?"T":"F")."</center></td>";
+			echo "<td><center>".(!$y?"T":"F")."</center></td>";
+			echo "<td><center>".($x&&$y?"T":"F")."</center></td>";
+			echo "<td><center>".($x||$y?"T":"F")."</center></td>";
+			echo "<td><center>".($x^$y?"T":"F")."</center></td>";
+			echo "<td><center>".($x^$y^$y?"T":"F")."</center></td>";
+			echo "<td><center>".($x^$y^$x?"T":"F")."</center></td>";
+			echo "<td><center>".(!($x&&$y)?"T":"F")."</center></td>";
+			echo "<td><center>".(!$x||!$y?"T":"F")."</center></td>";
+			echo "<td><center>".(!($x||$y)?"T":"F")."</center></td>";
+			echo "<td><center>".(!$x&&!$y?"T":"F")."</center></td></tr>";
 		?>
 	  	<?php // Echo out the F,F row.
 		 // $x is still false from former initialization.
 			$y=false;
 			
-			echo "<tr><td>".($x?"T":"F")."</td>";
-			echo "<td>".($y?"T":"F")."</td>";
-			echo "<td>".(!$x?"T":"F")."</td>";
-			echo "<td>".(!$y?"T":"F")."</td>";
-			echo "<td>".($x&&$y?"T":"F")."</td>";
-			echo "<td>".($x||$y?"T":"F")."</td>";
-			echo "<td>".($x^$y?"T":"F")."</td>";
-			echo "<td>".($x^$y^$y?"T":"F")."</td>";
-			echo "<td>".($x^$y^$x?"T":"F")."</td>";
-			echo "<td>".(!($x&&$y)?"T":"F")."</td>";
-			echo "<td>".(!$x||!$y?"T":"F")."</td>";
-			echo "<td>".(!($x||$y)?"T":"F")."</td>";
-			echo "<td>".(!$x&&!$y?"T":"F")."</td></tr>";
+			echo "<tr><td><center>".($x?"T":"F")."</center></td>";
+			echo "<td><center>".($y?"T":"F")."</center></td>";
+			echo "<td><center>".(!$x?"T":"F")."</center></td>";
+			echo "<td><center>".(!$y?"T":"F")."</center></td>";
+			echo "<td><center>".($x&&$y?"T":"F")."</center></td>";
+			echo "<td><center>".($x||$y?"T":"F")."</center></td>";
+			echo "<td><center>".($x^$y?"T":"F")."</center></td>";
+			echo "<td><center>".($x^$y^$y?"T":"F")."</center></td>";
+			echo "<td><center>".($x^$y^$x?"T":"F")."</center></td>";
+			echo "<td><center>".(!($x&&$y)?"T":"F")."</center></td>";
+			echo "<td><center>".(!$x||!$y?"T":"F")."</center></td>";
+			echo "<td><center>".(!($x||$y)?"T":"F")."</center></td>";
+			echo "<td><center>".(!$x&&!$y?"T":"F")."</center></td></tr>";
 			
 			// Close the table.
 			echo "</table></center>";
 		?>
 		<?php // Echo out notes.
 			// Exclusively OR notes.
-			echo "<enter><p><i>Note: <b>Exclusively OR</b></i></p></center>";
-			echo "<enter><p><i>X^Y = true if different, false if same.</i></p></center>";
-			echo "<enter><p><i>X^Y^Y = the 'Y's cancel each other out.</i></p></center>";
-			echo "<enter><p><i>X^Y^X = the 'X's cancel each other out.</i></p></center></i></p>";
+			echo "<center><p><i>Note: <b>Exclusively OR: </b>";
+			echo "X^Y = true if different, false if same; ";
+			echo "X^Y^Y = the 'Y's cancel each other out; ";
+			echo "X^Y^X = the 'X's cancel each other out.</i></p></center>";
 		?>
     </body>
 </html>
