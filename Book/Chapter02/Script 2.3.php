466aeaba5795601f955b9b61e6497d6f32520fdd
jl2286716 <jleduc@student.rcc.edu>
2014-09-13 14:24:56 -0700

    Signed-off-by: jl2286716 <jleduc@student.rcc.edu>

--- Book/Chapter02/Script 2.3.php
+++ Book/Chapter02/Script 2.3.php
@@ -8,20 +8,30 @@
     </head>
     
     <body>
-		<?php # Script 2.2 (pg42) - handle_form.php
+		<?php # Script 2.3 (pg46) - handle_form.php #2
 			// Create a shorthand for the form data:
 			$name = $_REQUEST['name'];
 			$email = $_REQUEST['email'];
 			$comments = $_REQUEST['comments'];
-			/* Not used:
-			$_REQUEST['age'];
-			$_REQUEST['gender'];
-			$_REQUEST['submit'];
-			*/
 			
+			// Create the $gender variable:
+			if (isset($_REQUEST['gender'])) {
+				$gender = $_REQUEST['gender'];
+			) else {
+				$gender = NULL;
+			}
 			// Print the submitted information:
 			echo "<p>Thank you, <b>$name</b>, for the following comments:<br /><tt>$comments</tt></p>";
 			echo "<p>We will reply to you at <i>$email</i>.</p>\n";
+			
+			// Print a message based upon the gender value:
+			if ($gender == 'M') {
+				echo '<p><b>Good day, Sir!</b></p>';
+			} elseif ($gender =='F') {
+				echo '<p><b>Good day, Madam!</b></p>';
+			} else { // No Gender selected.
+				echo '<p><b>You forgot to enter your gender!</b></p>';
+			}
 		?>
     </body>
 </html>
