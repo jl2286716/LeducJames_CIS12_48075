6024719ac1666402973eb72b83cf8853506398f2
jl2286716 <jleduc@student.rcc.edu>
2014-09-19 19:32:33 -0700

    Signed-off-by: jl2286716 <jleduc@student.rcc.edu>

---
+++ Hmwk/Assignment03/Chapter02 - Scripts/Script 2.3.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Form Feedback</title>
+		<style type="text/css" title="text/css" media="all">
+		</style>
+    </head>
+    
+    <body>
+		<?php # Script 2.3 (pg46) - handle_form.php #2
+			// Create a shorthand for the form data:
+			$name = $_REQUEST['name'];
+			$email = $_REQUEST['email'];
+			$comments = $_REQUEST['comments'];
+			
+			// Create the $gender variable:
+			if (isset($_REQUEST['gender'])) {
+				$gender = $_REQUEST['gender'];
+			) else {
+				$gender = NULL;
+			}
+			// Print the submitted information:
+			echo "<p>Thank you, <b>$name</b>, for the following comments:<br /><tt>$comments</tt></p>";
+			echo "<p>We will reply to you at <i>$email</i>.</p>\n";
+			
+			// Print a message based upon the gender value:
+			if ($gender == 'M') {
+				echo '<p><b>Good day, Sir!</b></p>';
+			} elseif ($gender =='F') {
+				echo '<p><b>Good day, Madam!</b></p>';
+			} else { // No Gender selected.
+				echo '<p><b>You forgot to enter your gender!</b></p>';
+			}
+		?>
+    </body>
+</html>
