df707958e8c849eb4581358cba82459eaf1d0c82
jl2286716 <jleduc@student.rcc.edu>
2014-10-13 13:57:30 -0700

    Signed-off-by: jl2286716 <jleduc@student.rcc.edu>

--- Hmwk/Assignment06/newGrade.sql
+++ Hmwk/Assignment06/newGrade.sql
@@ -27,7 +27,7 @@ DROP TABLE IF EXISTS `entity_assignments`;
 CREATE TABLE `entity_assignments` (
   `ass_id` int(10) NOT NULL,
   `name` varchar(50) DEFAULT NULL,
-  `points_possible` tinyint(3) DEFAULT NULL,
+  `points_possible` int(5) DEFAULT NULL,
   `weight` tinyint(3) DEFAULT NULL,
   `description` longtext,
   `start_date` date DEFAULT NULL,
@@ -42,7 +42,7 @@ CREATE TABLE `entity_assignments` (
 
 LOCK TABLES `entity_assignments` WRITE;
 /*!40000 ALTER TABLE `entity_assignments` DISABLE KEYS */;
-INSERT INTO `entity_assignments` VALUES (1,'HW1',NULL,NULL,'Homework 1',NULL,NULL),(2,'Q1',NULL,NULL,'Quiz 1',NULL,NULL),(3,'HW2',NULL,NULL,'Homework 2',NULL,NULL),(4,'Q2',NULL,NULL,'Quiz 2',NULL,NULL),(5,'HW3',NULL,NULL,'Homework 3',NULL,NULL),(6,'E1',NULL,NULL,'Exam 1: Midterm',NULL,NULL),(7,'HW4',NULL,NULL,'Homework 4',NULL,NULL),(8,'Q3',NULL,NULL,'Quiz 3',NULL,NULL),(9,'HW5',NULL,NULL,'Homework 5',NULL,NULL),(10,'Q4',NULL,NULL,'Quiz 4',NULL,NULL),(11,'HW6',NULL,NULL,'Homework 6',NULL,NULL),(12,'E2',NULL,NULL,'Exam 2: Final',NULL,NULL);
+INSERT INTO `entity_assignments` VALUES (1,'CHW1',70,7,'Homework 1',NULL,NULL),(2,'CQ1',50,5,'Quiz 1',NULL,NULL),(3,'CHW2',70,7,'Homework 2',NULL,NULL),(4,'CQ2',50,5,'Quiz 2',NULL,NULL),(5,'CHW3',70,7,'Homework 3',NULL,NULL),(6,'CMT',100,10,'Midterm',NULL,NULL),(7,'CHW4',80,8,'Homework 4',NULL,NULL),(8,'CQ3',50,5,'Quiz 3',NULL,NULL),(9,'CHW5',80,8,'Homework 5',NULL,NULL),(10,'CQ4',50,5,'Quiz 4',NULL,NULL),(11,'CHW6',80,8,'Homework 6',NULL,NULL),(12,'CF',150,15,'Final Exam',NULL,NULL),(13,'CFP',100,10,'Final Project',NULL,NULL),(14,'MF',250,25,'Final Exam',NULL,NULL),(15,'AQ1',60,6,'Quiz 1',NULL,NULL),(16,'AQ2',60,6,'Quiz 2',NULL,NULL),(17,'AQ3',60,6,'Quiz 3',NULL,NULL),(18,'AQ4',70,7,'Quiz 4',NULL,NULL),(19,'AMT',200,20,'Midterm',NULL,NULL),(20,'AQ5',60,6,'Quiz 5',NULL,NULL),(21,'AQ6',60,6,'Quiz 6',NULL,NULL),(22,'AQ7',60,6,'Quiz 7',NULL,NULL),(23,'AQ8',70,7,'Quiz 8',NULL,NULL),(24,'AFE',300,30,'Final Exam',NULL,NULL),(25,'MHW1',70,7,'Homework 1',NULL,NULL),(26,'MQ1',50,5,'Quiz 1',NULL,NULL),(27,'MHW2',70,7,'Homework 2',NULL,NULL),(28,'MQ2',50,5,'Quiz 2',NULL,NULL),(29,'HW3',70,7,'Homework 3',NULL,NULL),(30,'MMT',100,10,'Midterm',NULL,NULL),(31,'MHW4',80,8,'Homework 4',NULL,NULL),(32,'MQ3',50,5,'Quiz 3',NULL,NULL),(33,'MHW5',80,8,'Homework 5',NULL,NULL),(34,'MQ4',50,5,'Quiz 4',NULL,NULL),(35,'MHW6',80,8,'Homework 6',NULL,NULL);
 /*!40000 ALTER TABLE `entity_assignments` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -98,6 +98,7 @@ CREATE TABLE `entity_course` (
 
 LOCK TABLES `entity_course` WRITE;
 /*!40000 ALTER TABLE `entity_course` DISABLE KEYS */;
+INSERT INTO `entity_course` VALUES (200,'CIS',1,'A','Intro Comp Info Sys','Introduction to Computer Information Systems'),(201,'AST',1,'A','Intro Ast','Introduction to Astronomy'),(202,'MAT',11,NULL,'College Alg','College Algebra');
 /*!40000 ALTER TABLE `entity_course` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -130,7 +131,7 @@ CREATE TABLE `entity_instructor` (
 
 LOCK TABLES `entity_instructor` WRITE;
 /*!40000 ALTER TABLE `entity_instructor` DISABLE KEYS */;
-INSERT INTO `entity_instructor` VALUES (1150258,'Mark','E','Lehr','mark.lehr@rcc.edu',951,2228260,NULL,222,NULL,NULL),(1346852,'Scott','A','Blair','scott.blair@rcc.edu',951,5555679,NULL,208,NULL,NULL),(1649875,'Gabrielle','P','Mendoza','gabrielle.mendoza@rcc.edu',951,5559632,NULL,345,NULL,NULL),(2656485,'Jane','D','Somebody','yaddayadda@blah.com',714,5554568,NULL,156,NULL,NULL),(5686452,'Billy','S','Batson','bbat@shazam.com',909,5550562,NULL,325,NULL,NULL);
+INSERT INTO `entity_instructor` VALUES (1150258,'Mark','E','Lehr','mark.lehr@rcc.edu',951,2228260,NULL,222,NULL,NULL),(1346852,'Scott','A','Blair','scott.blair@rcc.edu',951,5555679,NULL,208,NULL,NULL),(1649875,'Gabrielle','P','Mendoza','gabrielle.mendoza@rcc.edu',951,5559632,NULL,345,NULL,NULL);
 /*!40000 ALTER TABLE `entity_instructor` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -160,7 +161,7 @@ CREATE TABLE `entity_student` (
 
 LOCK TABLES `entity_student` WRITE;
 /*!40000 ALTER TABLE `entity_student` DISABLE KEYS */;
-INSERT INTO `entity_student` VALUES (2286716,'James','A','Leduc','jleduc@student.rcc.edu',909,3635025,NULL),(2356897,'Joe','E','Student','jstu@student.rcc.edu',951,5556137,NULL);
+INSERT INTO `entity_student` VALUES (2286716,'James','A','Leduc','jleduc@student.rcc.edu',909,3635025,NULL);
 /*!40000 ALTER TABLE `entity_student` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -259,6 +260,7 @@ CREATE TABLE `xref_class_assignments` (
 
 LOCK TABLES `xref_class_assignments` WRITE;
 /*!40000 ALTER TABLE `xref_class_assignments` DISABLE KEYS */;
+INSERT INTO `xref_class_assignments` VALUES (1,48075,1),(2,48075,2),(3,48075,3),(4,48075,4),(5,48075,5),(6,48075,6),(7,48075,7),(8,48075,8),(9,48075,9),(10,48075,10),(11,48075,11),(12,48075,12),(13,48075,13),(14,48759,25),(15,48759,26),(16,48759,27),(17,48759,28),(18,48759,29),(19,48759,30),(20,48759,31),(21,48759,32),(22,48759,33),(23,48759,34),(24,48759,35),(25,48759,14),(26,48515,15),(27,48515,16),(28,48515,17),(29,48515,18),(30,48515,19),(31,48515,20),(32,48515,21),(33,48515,22),(34,48515,23),(35,48515,24);
 /*!40000 ALTER TABLE `xref_class_assignments` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -283,6 +285,7 @@ CREATE TABLE `xref_class_course` (
 
 LOCK TABLES `xref_class_course` WRITE;
 /*!40000 ALTER TABLE `xref_class_course` DISABLE KEYS */;
+INSERT INTO `xref_class_course` VALUES (215,48515,201),(259,48759,202),(275,48075,200);
 /*!40000 ALTER TABLE `xref_class_course` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -322,7 +325,7 @@ CREATE TABLE `xref_student_assignments` (
   `xref_stu_ass` int(10) NOT NULL,
   `student_id` int(10) DEFAULT NULL,
   `assignment_id` int(10) DEFAULT NULL,
-  `score` tinyint(3) DEFAULT NULL,
+  `score` int(10) DEFAULT NULL,
   `grade` char(2) DEFAULT NULL,
   `comments` varchar(150) DEFAULT NULL,
   PRIMARY KEY (`xref_stu_ass`)
@@ -335,6 +338,7 @@ CREATE TABLE `xref_student_assignments` (
 
 LOCK TABLES `xref_student_assignments` WRITE;
 /*!40000 ALTER TABLE `xref_student_assignments` DISABLE KEYS */;
+INSERT INTO `xref_student_assignments` VALUES (501,2286716,1,50,NULL,NULL),(502,2286716,2,40,NULL,NULL),(503,2286716,3,68,NULL,NULL),(504,2286716,4,46,NULL,NULL),(505,2286716,5,66,NULL,NULL),(506,2286716,6,88,NULL,NULL),(507,2286716,7,74,NULL,NULL),(508,2286716,8,50,NULL,NULL),(509,2286716,9,78,NULL,NULL),(510,2286716,10,48,NULL,NULL),(511,2286716,11,80,NULL,NULL),(512,2286716,12,150,NULL,NULL),(513,2286716,13,100,NULL,NULL),(514,2286716,25,60,NULL,NULL),(515,2286716,26,44,NULL,NULL),(516,2286716,27,70,NULL,NULL),(517,2286716,28,50,NULL,NULL),(518,2286716,29,68,NULL,NULL),(519,2286716,30,98,NULL,NULL),(520,2286716,31,80,NULL,NULL),(521,2286716,32,50,NULL,NULL),(522,2286716,33,72,NULL,NULL),(523,2286716,34,46,NULL,NULL),(524,2286716,35,76,NULL,NULL),(525,2286716,14,245,NULL,NULL),(526,2286716,15,40,NULL,NULL),(527,2286716,16,44,NULL,NULL),(528,2286716,17,50,NULL,NULL),(529,2286716,18,64,NULL,NULL),(530,2286716,19,175,NULL,NULL),(531,2286716,20,50,NULL,NULL),(532,2286716,21,42,NULL,NULL),(533,2286716,22,58,NULL,NULL),(534,2286716,23,66,NULL,NULL),(535,2286716,24,260,NULL,NULL);
 /*!40000 ALTER TABLE `xref_student_assignments` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -347,4 +351,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-10-08 21:00:33
+-- Dump completed on 2014-10-09 19:29:34
