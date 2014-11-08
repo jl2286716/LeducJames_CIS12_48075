48d857b56d9e66438e9bd54d34335fcd73ce9af6
jl2286716 <jleduc@student.rcc.edu>
2014-11-07 22:38:55 -0800

    Signed-off-by: jl2286716 <jleduc@student.rcc.edu>

--- Proj/TowerGuard/connect.php
+++ Proj/TowerGuard/connect.php
@@ -3,7 +3,7 @@
 	$username = "48075";
 	$password = "48075cis12";
 	$hostname = "209.129.8.5"; 
-	$db = "48075";
+	$db = "jl2286716_proj_entity_users";
 
 	$link = mysqli_connect($hostname,$username,$password,$db) or die("Error " . mysqli_error($link)); 
 ?>
