f8f984994190f0ec8255fbdea322c0d9e9a92d09
jl2286716 <jleduc@student.rcc.edu>
2014-11-16 03:09:50 -0800

    Signed-off-by: jl2286716 <jleduc@student.rcc.edu>

---
+++ Proj/Game/index.php
@@ -0,0 +1,92 @@
+<!--
+	Author:		James Leduc
+	Title:		Zompocalypse
+	Purpose:	A zombie game where survival is based upon choice and chance.
+	Created:	Nov 16, 2014
+	Modified:	Nov 16, 2014
+-->
+<!doctype html>
+<html lang="en">
+	<head>
+		<title>Zompocalypse Login</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="styles/styles.css">	<!-- Include Style Sheets -->
+		<script src=""></script>
+		<script>
+		</script>
+		<?php	//	Form Validations:
+			function validateName(){
+				$name = $_REQUEST['uName'];
+				$regX = "/^\s*([A-Z][a-z]{1,})(([A-Z][a-z]{1,})*)\s*$/";
+				if(preg_match($regX,$name)){
+					return true;
+				}else{
+					echo ("<script>alert('Please, enter a valid name!');</script>");
+					return false;
+				}
+			}
+			function validateEmail(){
+				$eMail = $_REQUEST['uMail'];
+				$regX = "/^\s*[A-Za-z0-9]{1,}[@][A-Za-z]{1,10}[\.][A-Za-z]{1,5}([\.][A-Za-z]{1,5})?\s*$/";
+				if(preg_match($regX,$eMail)){
+					return true;
+				}else{
+					echo ("<script>alert('Please, enter a valid email!');</script>");
+					return false;
+				}
+			}
+			function validatePassword(){
+				$passW = $_REQUEST['uPass'];
+				$regX = "/((?=.*\d)(?=.*[A-Za-z])(?=.*[!@#$%^&*-_]).{5,15})/";
+				if(preg_match($regX,$passW)){
+					return true;
+				}else{
+					echo ("<script>alert('Invalid Password!\nPlease, enter a password with a length of 5-15 characters!\nInclude at least 1 letter, 1 number, and 1 special character (!@#$%^&*-_)!');</script>");
+					return false;
+				}
+			}
+			function verifyPassword(){
+				$passW = $_REQUEST['uPass'];
+				$vPass = $_REQUEST['vPass'];
+				if($vPass==$passW){
+					return true;
+				}else{
+					echo ("<script>alert('Your passwords do not match!\nPlease, check your passwords!');</script>");
+					return false;
+				}
+			}
+			function validateForm(){
+				if(!validateName())return false;
+				if(!validateEmail())return false;
+				if(!validatePassword())return false;
+				if(!verifyPassword())return false;
+				return true;
+			}
+		?>
+	</head>
+	<body>		
+		<br><br><center><h1>LOGIN TO ENTER THE ZOMBIE APOCALYPSE!</h1></center><br><br>
+		<!--	User Form	-->
+		<center><div id="sign" style="">
+			<h2>SIGN UP</h2>
+			<form name="user" id="user" action="signup.php" onsubmit="return validateForm()" method="get">
+				<b>NAME:</b><br><input type="text" name="uName" id="uName" /><br>
+				<b>EMAIL:</b><br><input type="text" name="uMail" id="uMail" /><br>
+				<b>PASSWORD:</b><br><input type="text" name="uPass" id="uPass" /><br>
+				<b>VERIFY PASSWORD:</b><br><input type="text" name="vPass" id="vPass" /><br><br>
+				<input type="submit" value="SIGN UP" />
+			</form>
+		</div></center>
+		
+		<!--	Login Form	-->
+		<center><div id="log" style="">
+			<h2>LOGIN</h2>
+			<form name="login" id="login" action="login.php" method="post">
+				<b>EMAIL:</b><br><input type="text" name="logMail" id="logMail" /><br>
+				<b>PASSWORD:</b><br><input type="text" name="logPass" id="logPass"	/><br><br>
+				<input type="submit" value="LOGIN" />
+			</form>
+		</div></center>
+		
+	</body>
+</html>
