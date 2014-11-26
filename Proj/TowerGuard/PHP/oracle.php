55096c2255405762373ba8fd85ce2204863c4f55
jl2286716 <jleduc@student.rcc.edu>
2014-11-25 17:01:25 -0800

    Signed-off-by: jl2286716 <jleduc@student.rcc.edu>

--- Proj/TowerGuard/PHP/oracle.php
+++ Proj/TowerGuard/PHP/oracle.php
@@ -4,41 +4,152 @@
 </br>
 <center><h3>"We're Listening"</h3></center>
 
+<center>
+	<hr id="tLine" style="display:none">
+	<div id="errors" style="color:red"></div>
+	<hr id="bLine" style="display:none">
+</center>
+
+<!--	Form Validators	-->
+<?php
+	if($_SERVER['REQUEST_METHOD'] == 'POST'){
+	
+		$errors = array();
+	
+		//	Check first name:
+		$fname = $_POST['fName'];
+		$regX = "/^\s*([A-Z][a-z]{1,})(([A-Z][a-z]{1,})*)\s*$/";
+		
+		if(empty($_POST['fName'])){
+			echo ("<script>var tagID = document.getElementById('fName'); tagID.style.backgroundColor='#FF0000';</script>");
+			$errors[] = ("You forgot to enter your name!");
+		}elseif(preg_match($regX,$name)){
+			echo ("<script>var tagID = document.getElementById('fName'); tagID.style.backgroundColor='#00FF00';</script>");
+			$fn = trim($_POST['fName']);	//	$fn - 'firstname' for SQL query
+		}else{
+			echo ("<script>var tagID = document.getElementById('fName'); tagID.style.backgroundColor='#FF0000';</script>");
+			$errors[] = 'Please, enter a valid name!';
+		}
+		
+		//	Check last name:
+		$lname = $_POST['lName'];
+		$regX = "/^\s*([A-Z][a-z]{1,})(([A-Z][a-z]{1,})*)\s*$/";
+		
+		if(empty($_POST['lName'])){
+			echo ("<script>var tagID = document.getElementById('lName'); tagID.style.backgroundColor='#FF0000';</script>");
+			$errors[] = ("You forgot to enter your name!");
+		}elseif(preg_match($regX,$name)){
+			echo ("<script>var tagID = document.getElementById('lName'); tagID.style.backgroundColor='#00FF00';</script>");
+			$ln = trim($_POST['lName']);	//	$ln - 'lastname' for SQL query
+		}else{
+			echo ("<script>var tagID = document.getElementById('lName'); tagID.style.backgroundColor='#FF0000';</script>");
+			$errors[] = 'Please, enter a valid name!';
+		}
+		
+		//	Check email:
+		$eMail = $_POST['eMail'];
+		$regX = "/^\s*[A-Za-z0-9]{1,}[@][A-Za-z]{1,10}[\.][A-Za-z]{1,5}([\.][A-Za-z]{1,5})?\s*$/";
+		
+		if(empty($_POST['eMail'])){
+			echo ("<script>var tagID = document.getElementById('eMail'); tagID.style.backgroundColor='#FF0000';</script>");
+			$errors[] = "You forgot to enter your email!";
+		}elseif(preg_match($regX,$eMail)){
+			echo ("<script>var tagID = document.getElementById('eMail'); tagID.style.backgroundColor='#00FF00';</script>");
+			$m = trim($_POST['eMail']);	//	$m - 'email' for SQL query
+		}else{
+			echo ("<script>var tagID = document.getElementById('eMail'); tagID.style.backgroundColor='#FF0000';</script>");
+			$errors[] = 'Please, enter a valid email!';
+		}
+		
+		//	Check password:
+		$passW = $_POST['passW'];
+		$regX = "/((?=.*\d)(?=.*[A-Za-z])(?=.*[!@#$%^&*-_]).{5,15})/";
+		
+		if(empty($_POST['passW'])){
+			echo ("<script>var tagID = document.getElementById('passW'); tagID.style.backgroundColor='#FF0000';</script>");
+			$errors[] = "You forgot to enter your password!";
+		}elseif(preg_match($regX,$passW)){
+			echo ("<script>var tagID = document.getElementById('passW'); tagID.style.backgroundColor='#00FF00';</script>");
+			$p = trim($_POST['passW']);	//	$p - 'password' for SQL query
+		}else{
+			echo ("<script>var tagID = document.getElementById('passW'); tagID.style.backgroundColor='#FF0000';</script>");
+			$errors[] = 'Invalid Password! Please, enter a password with a length of 5-15 characters!';
+			$errors[] = 'Include at least 1 letter, 1 number, and 1 special character (e.g. !@#$%^&*-_)!';
+		}
+
+		//	Verify password:
+		$vPass = $_POST['vPass'];
+		
+		if(empty($_POST['vPass'])){
+			echo ("<script>var tagID = document.getElementById('vPass'); tagID.style.backgroundColor='#FF0000';</script>");
+			$errors[] = "You forgot to verify your password!";
+		}elseif($vPass==$passW){
+			echo ("<script>var tagID = document.getElementById('vPass'); tagID.style.backgroundColor='#00FF00';</script>");
+		}else{
+			echo ("<script>var tagID = document.getElementById('vPass'); tagID.style.backgroundColor='#FF0000';</script>");
+			$errors[] = 'Your passwords do not match!';
+			$errors[] = 'Please, recheck your passwords!';
+		}
+		
+		if(empty($errors)){	//	IF the form is filled out properly:
+		
+			//	Register to the database:
+			//	Open ('require') the database connection:
+			//	Make the query:
+			//	Run the query:
+				//	IF query ran okay, "You are registered!":
+				//	ELSE "System Error!":
+			//	Close database
+		
+		}else{	//	ELSE report the errors:
+			showErrors($errors);
+		}
+	}
+?>
+
+
 <center><form name="signup" id="signup"
 	action="oracle.php" onsubmit="return validate_form()" method="post">
 	<legend><fieldset>
+	
 		<fieldset id="left">
 			<legend>Basic Information</legend>
+			
 			<label>First Name*:<br>	<!-- color:#FF7E00 is a shade of Amber -->
 				<input type="text" name="fName" id="fName"
 					value="<?php if(isset($_POST['fName'])) echo $_POST['fName']; ?>"
 					style="background-color:#FF7E00; font-weight:bold"
 				/>
 			</label><br>
+			
 			<label>Last Name*:<br>
 				<input type="text" name="lName" id="lName"
 					value="<?php if(isset($_POST['lName'])) echo $_POST['lName']; ?>"
 					style="background-color:#FF7E00; font-weight:bold"
 				/>
 			</label><br>
+			
 			<label>Email*:<br>
 				<input type="text" name="eMail" id="eMail"
 					value="<?php if(isset($_POST['eMail'])) echo $_POST['eMail']; ?>"
 					style="background-color:#FF7E00; font-weight:bold"
 				/>
 			</label><br>
+			
 			<label>Password*:<br>
 				<input type="password" name="passW" id="passW" maxlength="20"
 					value="<?php if(isset($_POST['passW'])) echo $_POST['passW']; ?>"
 					style="background-color:#FF7E00; font-weight:bold"
 				/>
 			</label><br>
+			
 			<label>Verify Password*:<br>
 				<input type="password" name="vPass" id="vPass" maxlength="20"
 					value="<?php if(isset($_POST['vPass'])) echo $_POST['vPass']; ?>"
 					style="background-color:#FF7E00; font-weight:bold"
 				/>
 			</label><br>
+			
 			<label>Birth Date:<br>
 				<input type="text" name="bDay" id="bDay" maxlength="10"
 					value="<?php if(isset($_POST['bDay'])) echo $_POST['bDay']; ?>"
@@ -46,38 +157,31 @@
 				/>
 			</label><br>
 		</fieldset>
+		
 		<fieldset id="right">
 			<legend>Contact Information</legend>
-			<label>Street Number*:<br>
-				<input type="text" name="sNum" id="sNum" size="5"
-					value="<?php if(isset($_POST['sNum'])) echo $_POST['sNum']; ?>"
+			
+			<label>Address 1*:<br>
+				<input type="text" name="add1" id="add1" size="10"
+					value="<?php if(isset($_POST['add1'])) echo $_POST['add1']; ?>"
 					style="background-color:#FF7E00; font-weight:bold"
 				/>
 			</label><br>
-			<label>Street Name*:<br>
-				<input type="text" name="sName" id="sName"
-					value="<?php if(isset($_POST['sName'])) echo $_POST['sName']; ?>"
+			
+			<label>Address 2:<br>
+				<input type="text" name="add2" id="add2" maxlength="10"
+					value="<?php if(isset($_POST['add2'])) echo $_POST['add2']; ?>"
 					style="background-color:#FF7E00; font-weight:bold"
 				/>
 			</label><br>
-			<label>Street Type*:<br>
-				<input type="text" name="sType" id="sType" size="7"
-					value="<?php if(isset($_POST['sType'])) echo $_POST['sType']; ?>"
-					style="background-color:#FF7E00; font-weight:bold"
-				/>
-			</label><br>
-			<label>Apt. # or P.O. Box:<br>
-				<input type="text" name="aptPOB" id="aptPOB" maxlength="10"
-					value="<?php if(isset($_POST['aptPOB'])) echo $_POST['aptPOB']; ?>"
-					style="background-color:#FF7E00; font-weight:bold"
-				/>
-			</label><br>
+			
 			<label>City*:<br>
 				<input type="text" name="city" id="city"
 					value="<?php if(isset($_POST['city'])) echo $_POST['city']; ?>"
 					style="background-color:#FF7E00; font-weight:bold"
 				/>
 			</label><br>
+			
 			<label>State*:<br><!--use a drop down menu and list to validate-->
 				<select type="text" name="state" id="state" maxlength="2"
 					value="<?php if(isset($_POST['state'])) echo $_POST['state']; ?>"
@@ -136,24 +240,21 @@
 					<option value="WY">WY</option>
 				</select>
 			</label><br>
+			
 			<label>Zipcode*:<br>
 				<input type="text" name="zip" id="zip" maxlength="10" size="7"
 					value="<?php if(isset($_POST['zip'])) echo $_POST['zip']; ?>"
 					style="background-color:#FF7E00; font-weight:bold"
 				/>
 			</label><br>
-			<label>Area Code*:<br>
-				<input type="text" name="area" id="area" maxlength="3" size="3"
-					value="<?php if(isset($_POST['area'])) echo $_POST['area']; ?>"
-					style="background-color:#FF7E00; font-weight:bold"
-				/>
-			</label><br>
+			
 			<label>Phone Number*:<br>
 				<input type="text" name="phone" id="phone" maxlength="8" size="7"
 					value="<?php if(isset($_POST['phone'])) echo $_POST['phone']; ?>"
 					style="background-color:#FF7E00; font-weight:bold"
 				/>
 			</label><br>
+			
 		</fieldset>
 		<p>&nbsp;</p>
 		<center><input type="submit" value="Enter the TowerRealm"/>
