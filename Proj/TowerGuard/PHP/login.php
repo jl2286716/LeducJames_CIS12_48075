c6370c7890b96289e3a3c16f4edabf9bb7baab4d
jl2286716 <jleduc@student.rcc.edu>
2014-11-30 16:19:54 -0800

    Signed-off-by: jl2286716 <jleduc@student.rcc.edu>

--- Proj/TowerGuard/PHP/login.php
+++ Proj/TowerGuard/PHP/login.php
@@ -6,27 +6,29 @@
 <center><h2>Guardians, Login!</h2></center>
 <br><br>
 
-<center><form name="uLog" id="uLog" action="" method="" >
+
+
+<center><form name="uLog" id="uLog" action="" method="get" >
 	<legend><fieldset>
 		<fieldset>
 			<legend>GUARDIAN LOGIN</legend><br>
 			EMAIL:<br>
-			<input type="text" name="logMail" id="logMail" value="" /><br>
+			<input type="text" name="logMail" id="logMail" value="<?php if(isset($_GET['logMail'])) echo $_GET['logMail']; ?>" /><br>
 			PASSWORD:<br>
-			<input type="password" name="logPass" id="logPass" value="" /><br><br>
+			<input type="password" name="logPass" id="logPass" value="<?php if(isset($_GET['logPass'])) echo $_GET['logPass']; ?>" /><br><br>
 			<input type="submit" value="LOGIN" /><br>
 		</fieldset>
 	</fieldset></legend>
 </form></center>
 
-<center><form name="aLog" id="aLog" action="dash.php" method="" >
+<center><form name="aLog" id="aLog" action="dash.php" method="get" >
 	<legend><fieldset>
 		<fieldset>
 			<legend>ADMIN LOGIN</legend><br>
 			USER:<br>
-			<input type="text" name="admMail" id="admMail" value="" /><br>
+			<input type="text" name="admMail" id="admMail" value="<?php if(isset($_GET['admMail'])) echo $_GET['admMail']; ?>" /><br>
 			PASSWORD:<br>
-			<input type="password" name="admPass" id="admPass" value="" /><br><br>
+			<input type="password" name="admPass" id="admPass" value="<?php if(isset($_GET['admPass'])) echo $_GET['admPass']; ?>" /><br><br>
 			<input type="submit" value="LOGIN" /><br>
 		</fieldset>
 	</fieldset></legend>
