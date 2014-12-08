56c79ad027b0dcc917b3ec6a9a4ccb2ccd975e02
jl2286716 <jleduc@student.rcc.edu>
2014-12-08 02:16:11 -0800

    Signed-off-by: jl2286716 <jleduc@student.rcc.edu>

--- Proj/TowerGuard/PHP/checkout.php
+++ Proj/TowerGuard/PHP/checkout.php
@@ -1,9 +1,9 @@
 <?php include_once("../includes/header.php"); ?>
 <center><h1>CHECK OUT</h1></center>
 </br>
-<center><h3>"Here's your gear!"</h3></center>
+<center><h3>"Order Confirmation"</h3></center>
 <a href="cart.php"><button>CART</button></a>
-<script>
+<?php
 
-</script>
+?>
 <?php include_once("../includes/footer.php"); ?>
